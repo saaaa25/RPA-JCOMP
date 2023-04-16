6e2e8ef5c405907627da47be262c916a97e4f1f3
Ananya Dikshit <110302666+Dikshit-Ananya@users.noreply.github.com>
2023-04-16 19:50:30 +0530

    Add files via upload

---
+++ Website Files/InsidePage.js
@@ -0,0 +1,20 @@
+function clickon() {
+    window.location.href = 'index.html';
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+  // var name = ;
+}
+
+// function getName() {
+  var name = sessionStorage.getItem("Name");
+  document.getElementById('getname').innerHTML = "Hello, " + name + "!";
+// }
+
+  
